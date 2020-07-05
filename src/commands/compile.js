e9f5c39c9f05519ec6d6710bdd1eb2e479ac6a3e
GRFreire <gui.rugai.freire@gmail.com>
2020-07-05 02:49:34 -0300

    feat: add compile command

---
+++ src/commands/compile.js
@@ -0,0 +1,54 @@
+const compiler = require('../functions/compiler')
+
+const command = {
+  name: 'compile',
+  description: 'Compile your sBotics c# code',
+  alias: ['c'],
+  run: async toolbox => {
+    const {
+      template,
+      filesystem: { cwd, read },
+      print: { success, error, info }
+    } = toolbox
+
+    try {
+      const sbProj = await read('sbproj.json', 'json')
+
+      if (!sbProj) {
+        error('This folder has no sBotics project in it')
+        info('Type \'sboticscompiler init\' to generate a project')
+        return undefined
+      }
+
+      const { main, outputFolder } = sbProj
+
+      if (!main) {
+        error('There is no main file specified on your sbproj.json')
+        return undefined
+      }
+
+      if (!outputFolder) {
+        error('There is no outputFolder file specified on your sbproj.json')
+        return undefined
+      }
+
+      const programIndexPath = `${cwd()}/${sbProj.main}`.split('/')
+      const programIndexFile = programIndexPath.pop()
+      const programPath = programIndexPath.join('/')
+
+      const program = compiler(programPath, programIndexFile)
+
+      await template.generate({
+        template: 'main.cs.ejs',
+        target: `${outputFolder}/main.cs`,
+        props: { program }
+      })
+
+      success(`Successfully compiled!`)
+    } catch (err) {
+      error(err)
+    }
+  }
+}
+
+module.exports = command
