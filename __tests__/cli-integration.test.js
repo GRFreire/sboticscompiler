d9a1e831793a553f14c9530fc3dead64ac0088d1
GRFreire <gui.rugai.freire@gmail.com>
2020-07-07 22:08:28 -0300

    test: fix bad tests

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -2,6 +2,8 @@ const { system, filesystem } = require('gluegun')
 
 const src = filesystem.path(__dirname, '..')
 
+const lib = require('../package.json');
+
 const cli = async cmd =>
   system.run(
     'node ' + filesystem.path(src, 'bin', 'sboticscompiler') + ` ${cmd}`
@@ -9,10 +11,10 @@ const cli = async cmd =>
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(lib.version)
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(lib.version)
 })
